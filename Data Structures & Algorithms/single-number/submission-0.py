4e183b11ffbbed6e3f7ddbac912ed2870997f66c
Annas-Shariff <142386576+Annas-Shariff@users.noreply.github.com>
2026-06-23 18:36:41 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        freq = {}
+
+        for n in nums:
+            if n in freq:
+                freq[n] += 1
+            else:
+                freq[n] = 1
+
+        for n in freq:
+            if freq[n] == 1:
+                return n
